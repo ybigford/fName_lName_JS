a684984c40c36b67decfdaf1e6c382a908352b36
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-01-01 17:24:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let fName = document.getElementById ("fName");
+let lName = document.getElementById ("lName");
+let city = document.getElementById ("city");
+let school = document.querySelector ("school");
+
+fName.innerText = "Yapa";
+lName.innerText = "Bigford";
+city.innerText = "Seattle";
+school.innerText = "Seattle Central College";
